7f522970c25b52242661a1a5deb469765d843da0
Quyana Barrow <qbarrow@gmail.com>
2022-12-14 10:30:53 -0500

    homepage and folders

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+
+// import Nav from "./sub-components/Nav"
+// import Footer from "./sub-components/Footer"
+export default function About() {
+  return <>
+    <div>
+        <h2>this is the about page</h2>
+    </div>
+  </>
+  
+}
