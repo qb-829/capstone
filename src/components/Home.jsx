a2a01a2f7883cee056c8f31c56466fb6481c499a
T'Vedt Lazenby <tvedt@10downproductions.com>
2022-12-15 20:13:20 -0500

    initial push.. added LoginForm

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+// import React from 'react'
+
+// export default function Home() {
+//   return <>
+//   <div>Home</div>
+  
+//   </>
+// }
+
+
+import React from 'react';
+import LoginForm from './sub-components/LoginForm';
+import Background from "./assets/images/HomeBackground.jpeg"; 
+
+
 
 export default function Home() {
-  return <>
-  <div>Home</div>
-  
-  </>
+    return <>
+        <div className=' pb-5'>
+            <LoginForm />
+        </div>
+    </>
 }
+  
