28b191e2c01a86f70f4fffb296883f725612ce07
T'Vedt Lazenby <tvedt@10downproductions.com>
2022-12-22 19:32:30 -0500

    login form push

--- src/components/sub-components/LoginForm.jsx
+++ src/components/sub-components/LoginForm.jsx
@@ -32,11 +32,12 @@ export default function LoginForm() {
     
     //grab the username and password from the input fields
 
-    const handleClick = (e) => {
-        window.location.reload();
-    };
+    // const handleClick = (e) => {
+    //     window.location.reload();
+    // };
 
     const handleSubmit = (e) => {
+        console.log(username)
         e.preventDefault();
     
         setIsSubmitted(true);
@@ -49,13 +50,13 @@ export default function LoginForm() {
 
     const getRecords = async() => {
         // try catch block
-        let records = true, user = true; // for test only. Delete this when the db below is used
+        let records = true, username = true; // for test only. Delete this when the db below is used
         // const records = await Users.querry... // finish some cool code here
         // find the username and password
         // if records.length is > 0
         // check the password
         // if password matches
-        navigate('/protected-page', {
+        navigate('/Home', {
             state: {
                 username: username,
                 password: password,
@@ -95,13 +96,16 @@ export default function LoginForm() {
                             required
                         />
                     </div>
-                    <Button variant="contained" type='submit' className='btn btn-primary' id='btn-submit'>LOGIN</Button>
+                    <Button variant="contained" type='submit' className='btn btn-primary' id='btn-submit' onClick={() => handleSubmit()}>LOGIN</Button>
+                    
                 </div>
             
+                    <br /><h5>No Account? No problem! Sign up below!</h5>
 
-                    <br />
-                    <Button variant="contained" onClick={handleClick} className='btn btn-danger' id='btn-danger' >SIGN UP</Button>
+                    <Button variant="contained" className='btn btn-danger' id='btn-danger' onClick={event =>  window.location.href='/seeders.js'}>SIGN UP</Button>
+                    
             </form>
         </div>
   </>
 }
+
